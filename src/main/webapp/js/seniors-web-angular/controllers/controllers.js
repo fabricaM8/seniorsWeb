8e543a77d23d83cf1da17f2e97513d5afa731b2e
SilverPaul <paulo.al.mcz@gmail.com>
2015-02-19 14:22:07 -0300

    Alterações referentes ao detalhe de medicamentos - Prioridades coloridas - Datepicker - TimerPicker

--- src/main/webapp/js/seniors-web-angular/controllers/controllers.js
+++ src/main/webapp/js/seniors-web-angular/controllers/controllers.js
@@ -78,7 +78,76 @@ app.controller('MedicacaoDetailCtrl', function($scope, $rootScope, $routeParams,
 		id : $routeParams.id
 	});
 	
+	//DataPicker
+	 $scope.today = function() {
+		    $scope.dt = new Date();
+		  };
+		  $scope.today();
 
+		  $scope.clear = function () {
+		    $scope.dt = null;
+		  };
+
+		  // Disable weekend selection
+		  $scope.disabled = function(date, mode) {
+		    return"";
+		   // ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+		  };
+
+		  $scope.toggleMin = function() {
+		    $scope.minDate = $scope.minDate ? null : new Date();
+		  };
+		  $scope.toggleMin();
+		  
+		  $scope.open = function($event) {
+		    $event.preventDefault();
+		    $event.stopPropagation();
+
+		    $scope.opened = true;
+		  };
+
+		  $scope.dateOptions = {
+		    formatYear: 'yyyy',
+		    startingDay: 0,
+		    showWeeks:'false'
+		  };
+
+		  $scope.formats = ['dd - MMMM - yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate','fullDate'];
+		  $scope.format = $scope.formats[4];
+		//Fim DataPicker
+		  
+		  //timerpick
+		  $scope.mytime = new Date();
+
+		  $scope.hstep = 1;
+		  $scope.mstep = 15;
+
+		  $scope.options = {
+		    hstep: [1, 2, 3],
+		    mstep: [1, 5, 10, 15, 25, 30]
+		  };
+
+		  $scope.ismeridian = true;
+		  $scope.toggleMode = function() {
+		    $scope.ismeridian = ! $scope.ismeridian;
+		  };
+
+		  $scope.update = function() {
+		    var d = new Date();
+		    d.setHours( 1 );
+		    d.setMinutes( 0 );
+		    $scope.mytime = d;
+		  };
+
+		  $scope.changed = function () {
+		   // $log.log('Time changed to: ' + $scope.mytime);
+		  };
+
+		  $scope.clear = function() {
+		    $scope.mytime = null;
+		  };
+		  $scope.update();
+		  //fim timerpicker
 });
 
 app.controller('UserListCtrl', function($scope, $rootScope, UsersFactory, UserFactory, $location, $dialogs) {
